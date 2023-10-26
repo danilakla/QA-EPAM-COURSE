4bf03ec7a79d2b9f48cc450225c334f4c4afe2b6
Danila <67117334+danilakla@users.noreply.github.com>
2023-10-26 18:53:41 +0300

    lab 9 curse epam

---
+++ lab9/epamtask/bring_it_on/src/test/java/page/CreatedPaste.java
@@ -0,0 +1,47 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import waits.CustomCondition;
+
+import java.time.Duration;
+
+public class CreatedPaste {
+    private final int WAIT_TIME_IN_SECONDS = 30;
+    private final WebDriver driver;
+    @FindBy(xpath = "//div[contains(@class, 'top-buttons')]/div[contains(@class, 'right')]/a[contains(text(), 'raw')]")
+    private WebElement rawLink;
+
+    @FindBy(xpath = "//div[contains(@class,'highlighted-code')]/descendant::a")
+    private WebElement resultHighlighting;
+
+    @FindBy(xpath = "//textarea[contains(@class,'js-paste-raw')]")
+    private WebElement resultRawPaste;
+
+    @FindBy(xpath = "//pre")
+    private WebElement rawPaste;
+
+    public CreatedPaste(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    public String getHighlighting() {
+        return resultHighlighting.getText();
+    }
+
+    public String getRawPaste() {
+        rawLink.click();
+        String paste = rawPaste.getText().trim();
+        driver.navigate().back();
+        return paste;
+    }
+}
---
+++ lab9/epamtask/i_can_win/src/test/java/waits/CustomCondition.java
@@ -0,0 +1,27 @@
+package waits;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import javax.annotation.CheckForNull;
+
+public class CustomCondition {
+    public static ExpectedCondition<Boolean> jQueryAJAXsCompleted() {
+        return new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return (Boolean) ((JavascriptExecutor)driver).executeScript("return (window.jQuery !=null) && (jQuery.active===0);");
+            }
+        };
+    }
+
+    public static ExpectedCondition<Boolean> changingURL(String url) {
+        return new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return !driver.getCurrentUrl().equals(url);
+            }
+        };
+    }
+}
